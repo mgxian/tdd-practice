8f068b9e3915c23bc0d9d821223860ebc5ddffa0
mgxian <will835559313@163.com>
2019-06-08 16:59:08 +0800

    move test

--- task3/marsrover/marsover.go
+++ task3/marsrover/marsover.go
@@ -11,9 +11,36 @@ const (
 	West
 )
 
+// Postion is postion
+type Postion struct {
+	x int
+	y int
+}
+
 // MarsRover is mars rover.
 type MarsRover struct {
 	direction Direction
+	Postion
+	maxX int
+	maxY int
+}
+
+func (mr *MarsRover) setStartPostion(x, y int) {
+	mr.x = x
+	mr.y = y
+}
+
+func (mr *MarsRover) setDirection(d Direction) {
+	mr.direction = d
+}
+
+func (mr *MarsRover) limitArea(x, y int) {
+	mr.maxX = x
+	mr.maxY = y
+}
+
+func (mr *MarsRover) postion() Postion {
+	return mr.Postion
 }
 
 func (mr *MarsRover) turn90DegreeLeft() {
@@ -28,8 +55,39 @@ func (mr *MarsRover) turn90DegreeRight() {
 	mr.direction = (mr.direction + 1) % 4
 }
 
-func newMarsRover(d Direction) *MarsRover {
+func (mr *MarsRover) forward(d int) {
+	switch mr.direction {
+	case North:
+		mr.y += d
+	case East:
+		mr.x += d
+	case South:
+		mr.y -= d
+	case West:
+		mr.x -= d
+	}
+
+	if mr.x > mr.maxX {
+		mr.x = mr.maxX
+	}
+
+	if mr.y > mr.maxY {
+		mr.y = mr.maxY
+	}
+
+	if mr.x < 0 {
+		mr.x = 0
+	}
+
+	if mr.y < 0 {
+		mr.y = 0
+	}
+}
+
+func newMarsRover() *MarsRover {
 	mr := new(MarsRover)
-	mr.direction = d
+	mr.direction = North
+	mr.x = 0
+	mr.y = 0
 	return mr
 }
--- task3/marsrover/marsrover_test.go
+++ task3/marsrover/marsrover_test.go
@@ -1,10 +1,14 @@
 package marsrover
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestTurn(t *testing.T) {
 	t.Run("turn left 90 degrees", func(t *testing.T) {
-		marsRover := newMarsRover(North)
+		marsRover := newMarsRover()
+		marsRover.setDirection(North)
 		marsRover.turn90DegreeLeft()
 		assertDirection(t, marsRover.direction, West)
 		marsRover.turn90DegreeLeft()
@@ -14,7 +18,8 @@ func TestTurn(t *testing.T) {
 	})
 
 	t.Run("turn right 90 degrees", func(t *testing.T) {
-		marsRover := newMarsRover(North)
+		marsRover := newMarsRover()
+		marsRover.setDirection(North)
 		marsRover.turn90DegreeRight()
 		assertDirection(t, marsRover.direction, East)
 		marsRover.turn90DegreeRight()
@@ -24,7 +29,41 @@ func TestTurn(t *testing.T) {
 	})
 }
 
+func TestMove(t *testing.T) {
+	moveTests := []struct {
+		testName       string
+		maxX, maxY     int
+		startx, starty int
+		startDirection Direction
+		distance       int
+		endx, endy     int
+	}{
+		{"forward 3 from (1, 3) head north direction", 10, 10, 1, 3, North, 3, 1, 6},
+		{"forward 9 from (1, 3) head north direction", 10, 10, 1, 3, North, 9, 1, 10},
+		{"forward 3 from (1, 3) head east direction", 10, 10, 1, 3, East, 3, 4, 3},
+		{"forward 9 from (1, 3) head east direction", 10, 10, 1, 3, East, 9, 10, 3},
+	}
+	for _, tt := range moveTests {
+		t.Run(tt.testName, func(t *testing.T) {
+			marsRover := newMarsRover()
+			marsRover.limitArea(tt.maxX, tt.maxY)
+			marsRover.setStartPostion(tt.startx, tt.starty)
+			marsRover.setDirection(tt.startDirection)
+			marsRover.forward(tt.distance)
+			assertPostion(t, marsRover.postion(), Postion{tt.endx, tt.endy})
+		})
+	}
+}
+
+func assertPostion(t *testing.T, got, want Postion) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got (%d, %d), want (%d, %d)", got.x, got.y, want.x, want.y)
+	}
+}
+
 func assertDirection(t *testing.T, got, want Direction) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
 	}

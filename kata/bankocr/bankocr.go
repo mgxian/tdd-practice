4317356ba3600ab380fcbd3ead409edc4c68c0f8
mgxian <will835559313@163.com>
2019-07-03 11:05:01 +0800

    kata bank ocr user story 4

--- kata/bankocr/bankocr.go
+++ kata/bankocr/bankocr.go
@@ -71,31 +71,83 @@ const (
 `
 )
 
-func parseEntry(entry string) int {
+func addEntryNumber(entryNumber map[string]int, entry string, number int) {
+	entry = strings.ReplaceAll(entry, "\n", "")
+	entryNumber[entry] = number
+}
+
+func generateEntryNumber() map[string]int {
 	entryNumber := make(map[string]int, 0)
-	entryNumber[ENTRY0] = 0
-	entryNumber[ENTRY1] = 1
-	entryNumber[ENTRY2] = 2
-	entryNumber[ENTRY3] = 3
-	entryNumber[ENTRY4] = 4
-	entryNumber[ENTRY5] = 5
-	entryNumber[ENTRY6] = 6
-	entryNumber[ENTRY7] = 7
-	entryNumber[ENTRY8] = 8
-	entryNumber[ENTRY9] = 9
+	addEntryNumber(entryNumber, ENTRY0, 0)
+	addEntryNumber(entryNumber, ENTRY1, 1)
+	addEntryNumber(entryNumber, ENTRY2, 2)
+	addEntryNumber(entryNumber, ENTRY3, 3)
+	addEntryNumber(entryNumber, ENTRY4, 4)
+	addEntryNumber(entryNumber, ENTRY5, 5)
+	addEntryNumber(entryNumber, ENTRY6, 6)
+	addEntryNumber(entryNumber, ENTRY7, 7)
+	addEntryNumber(entryNumber, ENTRY8, 8)
+	addEntryNumber(entryNumber, ENTRY9, 9)
 
-	if !strings.HasPrefix(entry, "\n") {
-		entry = "\n" + entry
-	}
+	return entryNumber
+}
 
+func parseEntry(entry string) int {
+	entryNumber := generateEntryNumber()
+	entry = strings.ReplaceAll(entry, "\n", "")
 	if number, ok := entryNumber[entry]; ok {
 		return number
 	}
 	return -1
 }
 
-func parseStringLine(aStringLine string) []int {
+func smartParseEntry(entry string) []int {
 	result := make([]int, 0)
+	possibleNumbers := make(map[int]bool, 0)
+	entry = strings.ReplaceAll(entry, "\n", "")
+	number := parseEntry(entry)
+	possibleNumbers[number] = true
+
+	pipePostion := []int{3, 5, 6, 8}
+	underscopePostion := []int{1, 4, 7}
+	for _, pos := range pipePostion {
+		entryByte := []byte(entry)
+		if entryByte[pos] == ' ' {
+			entryByte[pos] = '|'
+			number := parseEntry(string(entryByte))
+			possibleNumbers[number] = true
+		}
+		if entryByte[pos] == '|' {
+			entryByte[pos] = ' '
+			number := parseEntry(string(entryByte))
+			possibleNumbers[number] = true
+		}
+	}
+
+	for _, pos := range underscopePostion {
+		entryByte := []byte(entry)
+		if entryByte[pos] == ' ' {
+			entryByte[pos] = '_'
+			number := parseEntry(string(entryByte))
+			possibleNumbers[number] = true
+		}
+		if entryByte[pos] == '_' {
+			entryByte[pos] = ' '
+			number := parseEntry(string(entryByte))
+			possibleNumbers[number] = true
+		}
+	}
+
+	for k := range possibleNumbers {
+		if k != -1 {
+			result = append(result, k)
+		}
+	}
+	return result
+}
+
+func splitEntry(aStringLine string) []string {
+	result := make([]string, 0)
 	offset := 0
 	numberLength := 3
 	numberLines := strings.Split(aStringLine, "\n")
@@ -108,16 +160,25 @@ func parseStringLine(aStringLine string) []int {
 		numberLine1 := numberLines[0][offset : offset+numberLength]
 		numberLine2 := numberLines[1][offset : offset+numberLength]
 		numberLine3 := numberLines[2][offset : offset+numberLength]
-		entry := fmt.Sprintf("%s\n%s\n%s\n", numberLine1, numberLine2, numberLine3)
+		entry := fmt.Sprintf("%s%s%s", numberLine1, numberLine2, numberLine3)
+		result = append(result, entry)
+		offset += numberLength
+	}
+
+	return result
+}
+
+func parseStringLine(aStringLine string) []int {
+	result := make([]int, 0)
+	for _, entry := range splitEntry(aStringLine) {
 		number := parseEntry(entry)
 		result = append(result, number)
-		offset += numberLength
 	}
 	return result
 }
 
-func parseNumbersFromFile(aFilePath string) [][]int {
-	result := make([][]int, 0)
+func splitStringLine(aFilePath string) []string {
+	result := make([]string, 0)
 	f, err := os.Open(aFilePath)
 	if err != nil {
 		return result
@@ -147,11 +208,20 @@ func parseNumbersFromFile(aFilePath string) [][]int {
 			lines = lines[:3]
 		}
 		stringLine := strings.Join(lines, "\n")
-		result = append(result, parseStringLine(stringLine))
+		result = append(result, stringLine)
 		if done {
 			return result
 		}
 	}
+}
+
+func parseNumbersFromFile(aFilePath string) [][]int {
+	result := make([][]int, 0)
+	for _, aStringLine := range splitStringLine(aFilePath) {
+		numbers := parseStringLine(aStringLine)
+		result = append(result, numbers)
+	}
+	return result
 }
 
 func validAccountNumbers(accountNumbers []int) bool {
@@ -186,5 +256,90 @@ func parseAndOutputEntry(aFilePath string, w io.Writer) {
 		result = append(result, []byte(status)...)
 		result = append(result, '\n')
 		w.Write(result)
+	}
+}
+
+func isCorrectedAccountNumbers(numbers []int) bool {
+	for _, number := range numbers {
+		if number == -1 {
+			return false
+		}
+	}
+	return true
+}
+
+func smartParseStringLine(aStringLine string) [][]int {
+	result := make([][]int, 0)
+	accountNumbers := parseStringLine(aStringLine)
+
+	for i, entry := range splitEntry(aStringLine) {
+		for _, number := range smartParseEntry(entry) {
+			numbers := make([]int, len(accountNumbers))
+			copy(numbers, accountNumbers)
+			numbers[i] = number
+			if isCorrectedAccountNumbers(numbers) {
+				result = append(result, numbers)
+			}
+		}
+	}
+	return result
+}
+
+func numbersString(numbers []int) string {
+	result := ""
+	for _, number := range numbers {
+		if number == -1 {
+			result += "?"
+			continue
+		}
+		result += strconv.Itoa(number)
+	}
+	return result
+}
+
+func smartParseAndOutputEntry(aFilePath string, w io.Writer) {
+	for _, line := range splitStringLine(aFilePath) {
+		status := ""
+		originAccountNumbers := ""
+		possibleAccountNumbers := make([]string, 0)
+		numbers := parseStringLine(line)
+		originAccountNumbers = numbersString(numbers)
+		if !strings.Contains(originAccountNumbers, "?") && validAccountNumbers(numbers) {
+			result := originAccountNumbers + "\n"
+			w.Write([]byte(result))
+			continue
+		}
+
+		for _, numbers := range smartParseStringLine(line) {
+			aNumbersString := numbersString(numbers)
+			if validAccountNumbers(numbers) {
+				possibleAccountNumbers = append(possibleAccountNumbers, aNumbersString)
+			}
+		}
+
+		if len(possibleAccountNumbers) == 0 {
+			status = "ILL"
+			result := originAccountNumbers + " " + status + "\n"
+			w.Write([]byte(result))
+			continue
+		}
+
+		if len(possibleAccountNumbers) == 1 {
+			result := possibleAccountNumbers[0] + "\n"
+			w.Write([]byte(result))
+			continue
+		}
+
+		if len(possibleAccountNumbers) > 1 {
+			status = "AMB"
+			result := originAccountNumbers + " " + status
+			result += " ["
+			for _, aNumbersString := range possibleAccountNumbers {
+				result += fmt.Sprintf("'%s', ", aNumbersString)
+			}
+			result = result[:len(result)-2]
+			result += "]\n"
+			w.Write([]byte(result))
+		}
 	}
 }
--- kata/bankocr/bankocr_test.go
+++ kata/bankocr/bankocr_test.go
@@ -2,6 +2,7 @@ package bankocr
 
 import (
 	"reflect"
+	"sort"
 	"strings"
 	"testing"
 )
@@ -54,12 +55,19 @@ func TestParseStringLine(t *testing.T) {
 }
 
 func TestParseNumbersFromFile(t *testing.T) {
-	aFilePath := "./0123456789.txt"
+	aFilePath := "./use-case-1.txt"
 	got := parseNumbersFromFile(aFilePath)
 	want := [][]int{
-		{1, 2, 3, 4, 5, 6, 7, 8, 9},
-		{9, 8, 7, 6, 5, 4, 3, 2, 1},
-		{9, -1, 7, 6, 5, 4, 3, 2, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{2, 2, 2, 2, 2, 2, 2, 2, 2},
+		{3, 3, 3, 3, 3, 3, 3, 3, 3},
+		{4, 4, 4, 4, 4, 4, 4, 4, 4},
+		{5, 5, 5, 5, 5, 5, 5, 5, 5},
+		{6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{7, 7, 7, 7, 7, 7, 7, 7, 7},
+		{8, 8, 8, 8, 8, 8, 8, 8, 8},
+		{9, 9, 9, 9, 9, 9, 9, 9, 9},
 	}
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
@@ -85,11 +93,139 @@ func TestValideAccountNumbers(t *testing.T) {
 
 func TestParseAndOutputEntry(t *testing.T) {
 	output := new(strings.Builder)
-	aFilePath := "./0123456789.txt"
+	aFilePath := "./use-case-3.txt"
 	parseAndOutputEntry(aFilePath, output)
 	got := output.String()
-	want := "123456789\n987654321 ERR\n9?7654321 ILL\n"
+	want := "000000051\n49006771? ILL\n1234?678? ILL\n"
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func TestSmartParseEntry(t *testing.T) {
+	tests := []struct {
+		entry string
+		want  []int
+	}{
+		{
+			`
+   
+  |
+  |
+`, []int{1, 7}},
+		{
+			`
+ _ 
+ _|
+|_ 
+`, []int{2}},
+		{
+			`
+ _ 
+ _|
+ _|
+`, []int{3, 9}},
+		{
+			`
+ _ 
+|_|
+|_|
+`, []int{0, 8, 9, 6}},
+	}
+
+	for _, tt := range tests {
+		want := tt.want
+		got := smartParseEntry(tt.entry)
+		assertSliceEqual(t, got, want)
+	}
+}
+
+func TestSmartParseAndOutputEntry(t *testing.T) {
+	output := new(strings.Builder)
+	aFilePath := "./use-case-4.txt"
+	smartParseAndOutputEntry(aFilePath, output)
+	got := output.String()
+	want := `711111111
+777777177
+200800000
+333393333
+888888888 AMB ['888886888', '888888988', '888888880']
+555555555 AMB ['559555555', '555655555']
+666666666 AMB ['686666666', '666566666']
+999999999 AMB ['899999999', '993999999', '999959999']
+490067715 AMB ['490867715', '490067719', '490067115']
+123456789
+000000051
+490867715
+`
+	gotEntry := strings.Split(got, "\n")
+	wantEntry := strings.Split(want, "\n")
+	if len(gotEntry) != len(wantEntry) {
+		t.Fatalf("got %d, want %d", len(gotEntry), len(wantEntry))
+	}
+
+	for i := 0; i < len(gotEntry); i++ {
+		assertEntryResult(t, gotEntry[i], wantEntry[i])
+	}
+}
+
+func assertEntryResult(t *testing.T, gotEntry, wantEntry string) {
+	gotEntryResult := strings.Split(gotEntry, " ")
+	wantEntryResult := strings.Split(wantEntry, " ")
+	if len(gotEntryResult) != len(wantEntryResult) {
+		t.Fatalf("got %d, want %d", len(gotEntryResult), len(wantEntryResult))
+	}
+	gotAccountNumbers := gotEntryResult[0]
+	wantAccountNumbers := wantEntryResult[0]
+	if gotAccountNumbers != wantAccountNumbers {
+		t.Errorf("got '%s', want '%s'", gotAccountNumbers, wantAccountNumbers)
+	}
+
+	if len(gotEntryResult) > 1 {
+		assertAccountNumbersStatus(t, gotEntryResult[1], wantEntryResult[1])
+	}
+
+	if len(gotEntryResult) > 2 {
+		assertPossibleAccountNumbers(t, gotEntryResult[2:], wantEntryResult[2:])
+	}
+}
+
+func assertPossibleAccountNumbers(t *testing.T, got, want []string) {
+	t.Helper()
+	gotPossibleAccountNumbers := make([]string, 0)
+	for _, numbers := range got {
+		numbers = strings.Trim(numbers, "[],")
+		gotPossibleAccountNumbers = append(gotPossibleAccountNumbers, numbers)
+	}
+	wantPossibleAccountNumbers := make([]string, 0)
+	for _, numbers := range want {
+		numbers = strings.Trim(numbers, "[],")
+		wantPossibleAccountNumbers = append(wantPossibleAccountNumbers, numbers)
+	}
+	sort.Strings(gotPossibleAccountNumbers)
+	sort.Strings(wantPossibleAccountNumbers)
+	if !reflect.DeepEqual(gotPossibleAccountNumbers, wantPossibleAccountNumbers) {
+		t.Errorf("got %v, want %v", gotPossibleAccountNumbers, wantPossibleAccountNumbers)
+	}
+}
+
+func assertAccountNumbersStatus(t *testing.T, got, want string) {
+	t.Helper()
+	assertString(t, got, want)
+}
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func assertSliceEqual(t *testing.T, got, want []int) {
+	t.Helper()
+	sort.Ints(got)
+	sort.Ints(want)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
